b460f5d4c52fd3c598929d2bf05832b192091a7f
Anvar Kalykov <kalykov.anvar@gmail.com>
2021-05-11 22:13:43 +0600

    Battlefield1-5

---
+++ src/Battlefield1/Game.java
@@ -0,0 +1,8 @@
+package Battlefield1;
+
+public class Game {
+    public void start() {
+        Battlefield battlefield = new Battlefield();
+        battlefield.initField();
+    }
+}
---
+++ src/Battlefield1/Main.java
@@ -0,0 +1,10 @@
+package Battlefield1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Write your code here
+        Game game = new Game();
+        game.start();
+
+    }
+}
